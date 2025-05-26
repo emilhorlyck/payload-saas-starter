381f3e917a98017266c82c2e58d5153f69695f91
Emil Hørlyck <emilha@gmail.com>
2025-05-26 20:24:35 +0200

    refactor: add Separator component to navbar

--- src/components/site/header.tsx
+++ src/components/site/header.tsx
@@ -8,36 +8,45 @@ import Logo from '@/public/logo.svg'
 import { getUser } from '@/lib/auth'
 
 import type { User } from '@/payload-types'
+import { Separator } from '../ui/separator'
 
 export const Header = async () => {
   const user: User | null = await getUser()
 
   return (
-    <nav className="flex justify-between items-center gap-4">
-      <Link href="/" className="flex gap-3 items-center">
-        <Image src={Logo} width={14} alt="Payload SaaS Starter" className="invert dark:invert-0" />
-        <h3 className="sm:text-lg">Payload SaaS Starter</h3>
-      </Link>
+    <>
+      <nav className="flex justify-between items-center gap-4 p-4">
+        <Link href="/" className="flex gap-3 items-center">
+          <Image
+            src={Logo}
+            width={14}
+            alt="Payload SaaS Starter"
+            className="invert dark:invert-0"
+          />
+          <h3 className="sm:text-lg">Payload SaaS Starter</h3>
+        </Link>
 
-      <div className="flex gap-2">
-        {user ? (
-          <>
-            <LogoutButton />
-            <Button asChild>
-              <Link href="/dashboard">Dashboard</Link>
-            </Button>
-          </>
-        ) : (
-          <>
-            <Button asChild variant="ghost">
-              <Link href="/login">Login</Link>
-            </Button>
-            <Button asChild>
-              <Link href="/register">Sign Up</Link>
-            </Button>
-          </>
-        )}
-      </div>
-    </nav>
+        <div className="flex gap-2">
+          {user ? (
+            <>
+              <LogoutButton />
+              <Button asChild>
+                <Link href="/dashboard">Dashboard</Link>
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button asChild variant="ghost">
+                <Link href="/login">Login</Link>
+              </Button>
+              <Button asChild>
+                <Link href="/register">Sign Up</Link>
+              </Button>
+            </>
+          )}
+        </div>
+      </nav>
+      <Separator />
+    </>
   )
 }
