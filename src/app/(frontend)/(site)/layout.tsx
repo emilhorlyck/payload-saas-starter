5bdc324a83930e86d6850ff4edef1e7b3dd650fe
Emil Hørlyck <emilha@gmail.com>
2025-05-27 08:02:38 +0100

    chore: replace navbar

--- src/app/(frontend)/(site)/layout.tsx
+++ src/app/(frontend)/(site)/layout.tsx
@@ -1,10 +1,12 @@
 import { Footer } from '@/components/site/footer'
 import { Header } from '@/components/site/header'
+import { Navbar } from '@/components/ui/NavBar'
 
 export default async function SiteLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
+      <Navbar />
       <main className="flex-1">{children}</main>
       <Footer />
     </>
